be24e035d27adf4489fec2704f2b9988e09bcea2
a1500956 <jukka.nilsson@myy.haaga-helia.fi>
2017-01-31 12:30:16 +0200

    Kirjojen listaus, syöttö sekä poisto toteutettu sekä tehty ulkoasua

--- src/main/java/com/bookstore/BookstoreApplication.java
+++ src/main/java/com/bookstore/BookstoreApplication.java
@@ -1,12 +1,35 @@
 package com.bookstore;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import com.bookstore.domain.Book;
+import com.bookstore.domain.BookRepository;
 
 @SpringBootApplication
 public class BookstoreApplication {
 
+	@Bean
+	public CommandLineRunner demo(BookRepository repository){
+		
+	return(args)->{
+		
+		Book book1 = new Book((long) 1, "Kalevala", "Elias Lönnrot", 2014, "978-1505723847", 12.99);
+		repository.save(book1);
+		Book book2 = new Book((long) 2, "Moby Dick", "Herman Melville", 1977, "978-3401036397", 24.99);
+		repository.save(book2);
+		Book book3 = new Book((long) 3, "Odyssey", "Homer", 1999, "978-0140268867", 9.99);
+		repository.save(book3);
+
+	};
+	}
+	
 	public static void main(String[] args) {
 		SpringApplication.run(BookstoreApplication.class, args);
 	}
+	
+	
 }
+
--- src/main/java/com/bookstore/domain/Book.java
+++ src/main/java/com/bookstore/domain/Book.java
@@ -1,75 +1,106 @@
 package com.bookstore.domain;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+
 public class Book {
 	
-	String title;
-	String author;
-	int year;
-	String isbn;
-	double price;
+	@Id
+    @GeneratedValue(strategy=GenerationType.AUTO)
+	
+	private Long id;
+	private String title;
+	private String author;
+	private int year;
+	private String isbn;
+	private double price;
 	
 	
-	public Book() {
-		super();
-		
+	
+	public Long getId() {
+		return id;
 	}
 
-	public Book(String title, String author, int year, String isbn, double price) {
-		super();
-		this.title = title;
-		this.author = author;
-		this.year = year;
-		this.isbn = isbn;
-		this.price = price;
+
+	public void setId(Long id) {
+		this.id = id;
 	}
+
 
 	public String getTitle() {
 		return title;
 	}
 
+
 	public void setTitle(String title) {
 		this.title = title;
 	}
+
 
 	public String getAuthor() {
 		return author;
 	}
 
+
 	public void setAuthor(String author) {
 		this.author = author;
 	}
+
 
 	public int getYear() {
 		return year;
 	}
 
+
 	public void setYear(int year) {
 		this.year = year;
 	}
+
 
 	public String getIsbn() {
 		return isbn;
 	}
 
+
 	public void setIsbn(String isbn) {
 		this.isbn = isbn;
 	}
+
 
 	public double getPrice() {
 		return price;
 	}
 
+
 	public void setPrice(double price) {
 		this.price = price;
 	}
 
+
+	public Book(){}
+		
+
+	public Book(Long id, String title, String author, int year, String isbn, double price) {
+		
+		this.id = id;
+		this.title = title;
+		this.author = author;
+		this.year = year;
+		this.isbn = isbn;
+		this.price = price;
+	}
+
+
 	@Override
 	public String toString() {
-		return "Book [title=" + title + ", author=" + author + ", year=" + year
-				+ ", isbn=" + isbn + ", price=" + price + "]";
+		return "Book [id=" + id + ", title=" + title + ", author=" + author
+				+ ", year=" + year + ", isbn=" + isbn + ", price=" + price
+				+ "]";
 	}
-	
-	
-	
 
+	
 }
